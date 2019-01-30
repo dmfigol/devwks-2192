83963f47587fb99d1570efd781e439bc347dd0d4
Dmitry Figol <git@dmfigol.me>
2019-01-30 09:01:31 +0100

    Prepare for CLEUR2019

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+sed -i "" '/198.18.1/d' ${HOME}/.ssh/known_hosts
+docker kill $(docker ps -q)
+docker pull dmfigol/jupyter-netdevops:cleur-2019
+docker run -it --rm -p 58888:58888 -v ${PWD}/jupyter:/jupyter/ --name=devwks-2192 dmfigol/jupyter-netdevops:cleur-2019
